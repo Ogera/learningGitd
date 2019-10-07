2f2545d064439b867bd1ee63e4c10608f946c540
Ogera <bruceogera10961@gmail.com>
2019-10-07 18:56:03 +0300

    first commit

---
+++ README.ml
@@ -0,0 +1,6 @@
+#Version Control System
+  allow management of source code overtime
+  form of backup for projects(source code)
+  speed up development process as a project can be worked on easily by several developers
+  Most commonly used vcsis Git
+Github used for hosting service for git repositories
